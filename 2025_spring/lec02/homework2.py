a51bb2c519d41743e217d5e993a7908aa1ebc878
Rainbow0512 <zhhena12@gmail.com>
2025-07-27 14:30:44 +0900

    Update homework2.py

--- 2025_spring/lec02/homework2.py
+++ 2025_spring/lec02/homework2.py
@@ -1,14 +1,14 @@
 
-a = None
-b = None
-c = None
-name1 = None
-name2 = None
-d = None
-e = None
-f = None
-g = None
-h = None
-i = None
-j = None
-k = None
+a = "This is a python string"
+b = 7 % 3
+c = 55 * 234
+name1 = "KCGI"
+name2 = "Kyoto"
+d = name1 + ' @ ' + name2
+e = 3.46
+f = '57'
+g = bool(0)
+h = str(-23.0)
+i = float(False)
+j = bool("Python")
+k = "print2" + "_squ"
