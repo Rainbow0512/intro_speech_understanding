ae476d42a6bc90ef12550f4dcd2ebc0889ab860a
Rainbow0512 <zhhena12@gmail.com>
2025-07-27 15:08:54 +0900

    Update homework5.py

--- 2025_spring/lec05/homework5.py
+++ 2025_spring/lec05/homework5.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 def center_of_gravity(x):
     '''
@@ -16,7 +15,14 @@ def center_of_gravity(x):
     @result:
     c (scalar): x's center of gravity
     '''
-    c = 0  # change this line
+    indices = np.arange(len(x))
+
+    numerator = np.dot(indices, x)
+
+    denominator = np.sum(x)
+
+    c = numerator / denominator
+
     return c
 
 def matched_identity(x):
@@ -30,7 +36,10 @@ def matched_identity(x):
     @result:
     I (array): a 2d numpy array: an NxN identity matrix
     '''
-    I =  0 # change this line
+    N = len(x)
+
+    I = np.eye(N)
+    
     return I
 
 def sine_and_cosine(t_start, t_end, t_steps):
@@ -48,9 +57,10 @@ def sine_and_cosine(t_start, t_end, t_steps):
     x (array of length t_steps): cos(t)
     y (array of length t_steps): sin(t)
     '''
-    # change these lines
-    t = 0 
-    x = 0
-    y = 0
-    # end changes here
+    t = np.linspace(t_start, t_end, t_steps)
+
+    x = np.cos(t)
+
+    y = np.sin(t)
+
     return t, x, y
